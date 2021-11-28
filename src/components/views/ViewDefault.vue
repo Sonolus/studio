9fc4f31e8e007f648dad97c99da915ed9522a0c3
NonSpicyBurrito <burritoyangster@gmail.com>
2021-11-28 16:22:10 +0800

    Increased version number to 0.3.0

--- src/components/views/ViewDefault.vue
+++ src/components/views/ViewDefault.vue
@@ -1,4 +1,3 @@
-import IconList from '../../icons/list-solid.svg?component';
 <script setup lang="ts">
 import IconList from '../../icons/list-solid.svg?component'
 </script>
@@ -14,7 +13,7 @@ import IconList from '../../icons/list-solid.svg?component'
         "
     >
         <div class="text-2xl font-semibold">Sonolus Studio</div>
-        <div>Version 0.2.1</div>
+        <div>Version 0.3.0</div>
         <div class="mt-16">
             Use
             <IconList class="icon inline-block ml-2 mr-1 my-2 sm:hidden" />
