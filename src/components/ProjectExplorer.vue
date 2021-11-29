e043de0df1e5eb25dfd1ab15d597ef3a964686ea
NonSpicyBurrito <burritoyangster@gmail.com>
2021-11-29 20:42:27 +0800

    Fixed skin sprite transformation initial value

--- src/components/ProjectExplorer.vue
+++ src/components/ProjectExplorer.vue
@@ -296,14 +296,14 @@ async function onNewSkinSprite(name: string) {
             bottom: true,
         },
         transform: {
-            x1: {},
-            x2: {},
-            x3: {},
-            x4: {},
-            y1: {},
-            y2: {},
-            y3: {},
-            y4: {},
+            x1: { x1: 1 },
+            x2: { x2: 1 },
+            x3: { x3: 1 },
+            x4: { x4: 1 },
+            y1: { y1: 1 },
+            y2: { y2: 1 },
+            y3: { y3: 1 },
+            y4: { y4: 1 },
         },
     })
 
