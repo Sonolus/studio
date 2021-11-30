5913b630350f366e2cc13051e5cb78a796aa1aa2
NonSpicyBurrito <burritoyangster@gmail.com>
2021-12-01 01:22:20 +0800

    Added skin sprite preview dragging

--- src/components/views/previews/PreviewSkinSprite.vue
+++ src/components/views/previews/PreviewSkinSprite.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import {
     useDevicePixelRatio,
-    useElementSize,
     useLocalStorage,
+    useMouseInElement,
+    useMousePressed,
 } from '@vueuse/core'
-import { computed, ref, watchEffect } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 import { Skin } from '../../../core/skin'
 import MyColorInput from '../../ui/MyColorInput.vue'
 import MyField from '../../ui/MyField.vue'
@@ -19,12 +20,18 @@ const backgroundColor = useLocalStorage(
 )
 
 const el = ref<HTMLCanvasElement>()
-const { width, height } = useElementSize(el, { width: 300, height: 300 })
+const { elementX, elementY, elementWidth, elementHeight } =
+    useMouseInElement(el)
+const { pressed } = useMousePressed({ target: el })
 const { pixelRatio } = useDevicePixelRatio()
 
 const context = computed(() => el.value?.getContext('2d'))
-const canvasWidth = computed(() => width.value * pixelRatio.value)
-const canvasHeight = computed(() => height.value * pixelRatio.value)
+const position = computed<Point>(() => [
+    (elementX.value * 2) / elementWidth.value - 1,
+    1 - (elementY.value * 2) / elementHeight.value,
+])
+const canvasWidth = computed(() => elementWidth.value * pixelRatio.value)
+const canvasHeight = computed(() => elementHeight.value * pixelRatio.value)
 
 type Point = [number, number]
 
@@ -35,6 +42,34 @@ const rect = ref<[Point, Point, Point, Point]>([
     [0.5, -0.5],
 ])
 
+const draggingIndex = ref<number>()
+const hoverIndex = computed(() => {
+    const [tx, ty] = position.value
+
+    const distances = rect.value
+        .map(([x, y], i) => [i, Math.hypot(tx - x, ty - y)])
+        .sort(([, a], [, b]) => a - b)
+
+    if (distances[0][1] > 20 / elementWidth.value) return
+
+    return distances[0][0]
+})
+
+watch(pressed, (value) => {
+    if (!value) {
+        draggingIndex.value = undefined
+        return
+    }
+
+    draggingIndex.value = hoverIndex.value
+})
+
+watchEffect(() => {
+    if (draggingIndex.value === undefined) return
+
+    rect.value[draggingIndex.value] = position.value
+})
+
 watchEffect(
     () => {
         const ctx = context.value
@@ -42,11 +77,8 @@ watchEffect(
 
         const w = canvasWidth.value
         const h = canvasHeight.value
-        ctx.setTransform(w / 2, 0, 0, h / 2, w / 2, h / 2)
-
-        ctx.lineWidth = 4 / w
-        ctx.strokeStyle = 'rgba(255, 255, 255, 0.125)'
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.125)'
+        ctx.setTransform(w / 2, 0, 0, -h / 2, w / 2, h / 2)
+        ctx.clearRect(-1, -1, 2, 2)
 
         const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = rect.value
 
@@ -57,13 +89,26 @@ watchEffect(
         ctx.lineTo(x4, y4)
         ctx.lineTo(x1, y1)
         ctx.closePath()
+        ctx.lineWidth = 4 / w
+        ctx.strokeStyle = 'rgba(255, 255, 255, 0.125)'
         ctx.stroke()
 
-        for (const [x, y] of rect.value) {
+        for (let i = 0; i < rect.value.length; i++) {
+            const [x, y] = rect.value[i]
             ctx.beginPath()
             ctx.arc(x, y, 0.02, 0, 2 * Math.PI)
             ctx.closePath()
+            ctx.fillStyle = getFillStyle(i)
             ctx.fill()
+        }
+
+        function getFillStyle(index: number) {
+            if (draggingIndex.value === index)
+                return 'rgba(255, 255, 255, 0.0625)'
+            if (draggingIndex.value === undefined && hoverIndex.value === index)
+                return 'rgba(255, 255, 255, 0.25)'
+
+            return 'rgba(255, 255, 255, 0.125)'
         }
     },
     { flush: 'post' }
@@ -79,12 +124,12 @@ watchEffect(
         />
     </MyField>
 
-    <div class="my-4 border-4 border-sonolus-ui-text-normal max-w-sm mx-auto">
+    <div class="max-w-sm mx-auto my-4 border-4 border-sonolus-ui-text-normal">
         <div class="relative h-0 pt-[100%] overflow-hidden">
             <canvas
                 ref="el"
-                class="absolute top-0 left-0 w-full h-full"
-                :style="{ backgroundColor }"
+                class="absolute top-0 left-0 w-full h-full select-none"
+                :style="{ backgroundColor, touchAction: 'none' }"
                 :width="canvasWidth"
                 :height="canvasHeight"
             />
