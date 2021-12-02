2ac38926a3e4f6e4e137bee61672195a4f3476a4
NonSpicyBurrito <burritoyangster@gmail.com>
2021-12-02 11:13:49 +0800

    Added displaying Sonolus version

--- src/components/views/ViewDefault.vue
+++ src/components/views/ViewDefault.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { version } from 'sonolus-core'
 import IconList from '../../icons/list-solid.svg?component'
 </script>
 
@@ -14,6 +15,7 @@ import IconList from '../../icons/list-solid.svg?component'
     >
         <div class="text-2xl font-semibold">Sonolus Studio</div>
         <div>Version 0.3.0</div>
+        <div class="text-xs">For Sonolus {{ version.sonolus }}</div>
         <div class="mt-16">
             Use
             <IconList class="icon inline-block ml-2 mr-1 my-2 sm:hidden" />
