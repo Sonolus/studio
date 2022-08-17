5cd3d18b554d2828965658b46f4a0798bb7461eb
NonSpicyBurrito <burritoyangster@gmail.com>
2022-08-17 12:19:56 +0800

    Updated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     mode: 'jit',
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false,
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {
             colors: {
