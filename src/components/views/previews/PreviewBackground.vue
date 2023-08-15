0b7ef4c6465557236fe805bcfc5dc4384f515bac
NonSpicyBurrito <burritoyangster@gmail.com>
2023-08-15 14:35:27 +0800

    Updated packages

--- src/components/views/previews/PreviewBackground.vue
+++ src/components/views/previews/PreviewBackground.vue
@@ -17,7 +17,7 @@ const aspectRatioValue = computed(
             '16:9': 16 / 9,
             '18:9': 18 / 9,
             '21:9': 21 / 9,
-        }[aspectRatio.value] || 16 / 9),
+        })[aspectRatio.value] || 16 / 9,
 )
 
 const width = computed(() => {
