8af20ad6827f3bc6e8a3a889760971abd139e481
Burrito <burritoyangster@gmail.com>
2024-04-19 08:37:05 +0800

    Refactored particle preview

--- src/components/views/previews/PreviewParticleEffect.vue
+++ src/components/views/previews/PreviewParticleEffect.vue
@@ -1,40 +1,38 @@
 <script setup lang="ts">
-import { computedAsync, useLocalStorage, useNow } from '@vueuse/core'
-import { computed, ref, watchPostEffect } from 'vue'
-import { useCanvas } from '../../../composables/canvas'
+import { computedAsync } from '@vueuse/core'
+import { computed, watchPostEffect } from 'vue'
+import { useParticlePreview } from '../../../composables/particle-preview'
 import { execute } from '../../../core/expression'
 import { Particle } from '../../../core/particle'
 import { renderParticle } from '../../../core/particle-renderer'
 import { ParticleState, getParticleState } from '../../../core/particle-state'
 import { getPropertyExpressionRandom } from '../../../core/property-expression'
 import { ImageInfo, Rect, getImageInfo } from '../../../core/utils'
-import IconRotate from '../../../icons/rotate.svg?component'
-import MyButton from '../../ui/MyButton.vue'
-import MyColorInput from '../../ui/MyColorInput.vue'
-import MyField from '../../ui/MyField.vue'
-import MyNumberInput from '../../ui/MyNumberInput.vue'
-import MyToggle from '../../ui/MyToggle.vue'
+import ParticlePreview from './ParticlePreview.vue'
 
 const props = defineProps<{
     sprites: Particle['data']['sprites']
     effect: Particle['data']['effects'][number]
 }>()
 
-const backgroundColor = useLocalStorage('preview.particleEffect.backgroundColor', '#000')
-const duration = useLocalStorage('preview.particleEffect.duration', 1)
-const loop = useLocalStorage('preview.particleEffect.loop', false)
+const {
+    loop,
+    progress,
 
-const now = useNow()
-const progress = computed(() => (now.value.valueOf() / 1000 / duration.value) % 1)
+    elBack,
+    elTop,
+    rect,
+    canvasWidth,
+    canvasHeight,
+    draggingIndex,
+    hoverIndex,
 
-const elBack = ref<HTMLCanvasElement>()
-const elTop = ref<HTMLCanvasElement>()
-const { rect, canvasWidth, canvasHeight, draggingIndex, hoverIndex } = useCanvas(elTop)
+    ctxBack,
+    ctxTop,
 
-const ctxBack = computed(() => elBack.value?.getContext('2d'))
-const ctxTop = computed(() => elTop.value?.getContext('2d'))
+    randomize,
+} = useParticlePreview()
 
-const randomize = ref(0)
 const randoms = computed(
     () => (
         randomize.value,
@@ -169,39 +167,12 @@ watchPostEffect(() => {
 </script>
 
 <template>
-    <MyField title="Background Color">
-        <MyColorInput
-            v-model="backgroundColor"
-            default-value="#000"
-            placeholder="Enter preview background color..."
-            validate
-        />
-    </MyField>
-    <MyField title="Duration">
-        <MyNumberInput v-model="duration" placeholder="Enter duration..." validate />
-    </MyField>
-    <MyField title="Loop">
-        <MyToggle v-model="loop" :default-value="true" />
-    </MyField>
-
-    <div class="mx-auto my-4 max-w-sm border-4 border-sonolus-ui-text-normal">
-        <div class="relative h-0 overflow-hidden pt-[100%]" :style="{ backgroundColor }">
-            <canvas
-                ref="elBack"
-                class="absolute left-0 top-0 h-full w-full"
-                :class="{ 'opacity-50': draggingIndex !== undefined }"
-                :width="canvasWidth"
-                :height="canvasHeight"
-            />
-            <canvas
-                ref="elTop"
-                class="absolute left-0 top-0 h-full w-full select-none opacity-50 hover:opacity-100"
-                :style="{ touchAction: 'none' }"
-                :width="canvasWidth"
-                :height="canvasHeight"
-            />
-        </div>
-    </div>
-
-    <MyButton class="mx-auto mt-4" :icon="IconRotate" text="Randomize" @click="randomize++" />
+    <ParticlePreview
+        v-model:el-back="elBack"
+        v-model:el-top="elTop"
+        v-model:randomize="randomize"
+        :canvas-width="canvasWidth"
+        :canvas-height="canvasHeight"
+        :dragging-index="draggingIndex"
+    />
 </template>
